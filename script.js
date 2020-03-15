a550f0331e9f35f81fbb017b5d92e9fc9bdf1956
Svetlana Kurbacheva <svetlana.a.k.15@mail.ru>
2020-03-15 21:29:29 +0300

    fix dom

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ const BUTTON = document.getElementById('btn');
 const CLOSE_BUTTON = document.getElementById('close-btn');
 const CHEVNEXT = document.getElementById('chevnext');
 const CHEVPREV = document.getElementById('chevprev');
-const WRAPSLIDER = document.getElementById('home');
+const WRAPSLIDER = document.getElementById('sliderwrap');
 const SCREENVERTICAL = document.getElementById('screenvertical')
 const SCREENHORIZONTAL = document.getElementById('screenhorizontal');
 const PHONEVERT = document.getElementById('blockvert');
 const PHONEHOR = document.getElementById('blockhor');
+const PORTFOLIOMENU = document.getElementById('portfoliomenu');
+const PORTFOLIOCONTENT = document.getElementById('portfoliocontent');
+let form = document.querySelector('form');
+
 
 MENU.addEventListener('click', (event) => {
     MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
     event.target.classList.add('active');
 })
+
 BUTTON.addEventListener('click', () => {
     const subjectname = document.getElementById('name').value.toString();
     const subjectemail = document.getElementById('email').value.toString();
@@ -27,13 +32,14 @@ BUTTON.addEventListener('click', () => {
     document.getElementById('message-block').classList.remove('hidden');
 
 })
+
 CLOSE_BUTTON.addEventListener('click', () => {
     document.getElementById('resultname').innerText = '';
     document.getElementById('resultemail').innerText = '';
     document.getElementById('result').innerText = '';
     document.getElementById('resultdescr').innerText = '';
     document.getElementById('message-block').classList.add('hidden');
-    /*  FormData.reset(); */
+
 })
 CHEVNEXT.addEventListener('click', () => {
     WRAPSLIDER.classList.toggle('slider');
@@ -51,3 +57,20 @@ PHONEHOR.addEventListener('click', () => {
     SCREENHORIZONTAL.classList.toggle('clearscreenh');
     SCREENHORIZONTAL.classList.toggle('screenhorizontal');
 })
+PORTFOLIOMENU.addEventListener('click', (event) => {
+    PORTFOLIOMENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
+    event.target.classList.add('active');
+    let img_list = PORTFOLIOCONTENT.querySelectorAll('img');
+    for (let c = 0; c < img_list.length; c++) {
+        img_list[img_list.length - 1].after(img_list[c]);
+    }
+})
+PORTFOLIOCONTENT.addEventListener('click', (event) => {
+    PORTFOLIOCONTENT.querySelectorAll('img').forEach(el => el.classList.remove('active'));
+    event.target.classList.add('active');
+})
+
+form.onsubmit = function(evt) {
+    evt.preventDefault();
+    FormData.reset();
+};
